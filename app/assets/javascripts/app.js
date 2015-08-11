5f1fd64ca312149a3bb2581d6088bedd58384430
KateWilkinson <kate.wilkinson@hotmail.com>
2015-08-11 10:16:40 +0100

    set up angular

---
+++ app/assets/javascripts/app.js
@@ -0,0 +1,1 @@
+var siteSearch = angular.module('SiteSearch', []);
---
+++ app/assets/javascripts/siteSearchController.js
@@ -0,0 +1,18 @@
+siteSearch.controller('SiteSearchController', [function() {
+
+var self = this;
+
+self.searchResult = {
+  "items": [
+    {
+      "name": "campingA",
+      "town": "Glastonbury"
+    },
+    {
+      "name": "campingB",
+      "town": "Rye"
+    }
+  ]
+};
+
+}]);
